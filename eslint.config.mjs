76159f52a9d6761a4f8256981058296c1ef535db
Haider Aftab <121552824+Haidercodehach@users.noreply.github.com>
2025-02-09 13:26:30 +0500

    Update ESLint configuration to extend Next.js recommended settings for core web vitals and TypeScript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,17 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+// Convert the current module URL into a file path
 const __filename = fileURLToPath(import.meta.url);
+// Get the directory name of the current file
 const __dirname = dirname(__filename);
 
+// Create a new FlatCompat instance with the current directory as the base
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// Extend the configuration using Next.js's recommended ESLint settings for core web vitals and TypeScript
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
